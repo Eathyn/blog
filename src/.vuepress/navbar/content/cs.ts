6a8b35b153a66a90d0c616a973ca45429da4252a
Eathyn <eathynzakas@gmail.com>
2023-09-24 11:50:05 +0800

    fix path

--- src/.vuepress/navbar/content/cs.ts
+++ src/.vuepress/navbar/content/cs.ts
@@ -1,6 +1,6 @@
 const cs = [
   { text: 'Data Structure and Algorithm', link: '/dsa/README.md' },
-  { text: 'Network', link: '/network/authentication/cookie/' },
+  { text: 'Network', link: '/network/cookie/' },
   { text: 'Database', link: '/database/postgresql/query_data/' },
   { text: 'Others', link: '/others/compile-time_vs_runtime' },
 ]
